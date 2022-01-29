6c86a341932f6bdb7c577ba023aab024e3437849
= <=>
2022-01-29 21:36:36 +0900

    Programmers 다리를 지나는 트럭
    - Queue를 이용한 풀이

---
+++ src/main/java/Programmers.java
@@ -0,0 +1,9 @@
+package src.main.java;
+
+public class Programmers {
+    public static void main(String[] args) {
+        TruckPassTheBridge truckPassTheBridge = new TruckPassTheBridge();
+        int[] weight_array = {10,10,10,10,10,10,10,10,10,10};
+        truckPassTheBridge.solution(100, 100, weight_array);
+    }
+}
